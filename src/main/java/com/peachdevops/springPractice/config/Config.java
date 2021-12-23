36320d08b51cdc51d3e35157d72bc2bbaf1cc379
Park <tij05078@gmail.com>
2021-12-23 23:48:47 +0900

    Config #1 - spring configuration

---
+++ src/main/java/com/peachdevops/springPractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.peachdevops.springPractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.peachdevops.springPractice")
+@Configuration
+public class Config {
+
+}
