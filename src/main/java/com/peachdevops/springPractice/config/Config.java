8b10c6f7b0af478855d755dbc6f7dc39d850ad9e
Park <tij05078@gmail.com>
2021-12-24 00:26:19 +0900

    Finish #1 - spring apply
    spring apply

--- src/main/java/com/peachdevops/springPractice/config/Config.java
+++ src/main/java/com/peachdevops/springPractice/config/Config.java
@@ -2,7 +2,9 @@ package com.peachdevops.springPractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.peachdevops.springPractice")
 @Configuration
 public class Config {
--- src/main/java/com/peachdevops/springPractice/service/SortService.java
+++ src/main/java/com/peachdevops/springPractice/service/SortService.java
@@ -13,7 +13,6 @@ public class SortService {
 
     public SortService(@Qualifier("bubbleSort") Sort<String> sort) {
         this.sort = sort;
-        System.out.println("구현: " + sort.getClass().getName());
     }
 
     public List<String> doSort(List<String> list) {
